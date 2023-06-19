c909381cfcce5802b508073b9973194828c78496
Stark Creed <itvhtonatiuh10@gmail.com>
2023-06-19 11:13:14 -0600

    Primer Commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,7 @@
+// import { crearDeck as crearDeckNuevo } from './usecases/create-deck';
+// import crearDeck, { miNombre } from './usecases/create-deck';
+export { crearCartaHTML } from './create-letter-html';
+export { crearDeck } from './create-deck';
+export { pedirCarta } from './request-letter';
+export { turnoComputadora } from './shift-computer';
+export { valorCarta } from './letter-value';
